3402c85cff7a293fce3b51e9cf95bb9c8e1f8d34
Sophia <sprice@overstock.com>
2018-10-24 09:58:04 +0100

    async example tests

--- src/components/__tests__/Example.test.js
+++ src/components/__tests__/Example.test.js
@@ -1,3 +1,5 @@
+import delay from "redux-saga";
+
 describe("The question list", () => {
   beforeEach(() => {
     console.log("Before each...");
@@ -15,9 +17,9 @@ describe("The question list", () => {
     console.log("After all..");
   });
 
-  it.only("Should display a list of items", () => {
-    expect(40 + 2).toEqual(42);
-  });
+  // it.only("Should display a list of items", () => {
+  //   expect(40 + 2).toEqual(42);
+  // });
 
   it("Should not display a single item", () => {
     expect(2 + 2).toEqual(4);
@@ -25,5 +27,19 @@ describe("The question list", () => {
 
   it.skip("Should not display single items", () => {
     expect(2 + 3).toEqual(4);
+  });
+
+  it("async test 1", done => {
+    setTimeout(done, 100);
+  });
+
+  it("async test 2", () => {
+    return new Promise(resolve => {
+      setTimeout(resolve, 500);
+    });
+  });
+
+  it("async test 3", async () => {
+    await delay(1000);
   });
 });
