00a9d4f09f6a84daf452f3c6e6260016afb9b087
Sophia <sprice@overstock.com>
2018-10-24 11:40:50 +0100

    stateless component test

--- src/components/__tests__/QuestionDetail.js
+++ src/components/__tests__/QuestionDetail.js
@@ -1,3 +1,40 @@
+import { mapStateToProps, QuestionDetailDisplay } from "../QuestionDetail";
+import renderer from "react-test-renderer";
+import React from "react";
+
 describe(`The Question Detail Component`, () => {
-  it(`Should not regress`, () => {});
+  describe(`The Container Element`, () => {
+    describe(`mapsStateToProps`, () => {
+      it("should map the state to props correctly", () => {
+        const sampleQuestion = {
+          question_id: 42,
+          body: "Space is big"
+        };
+        const appState = {
+          questions: [sampleQuestion]
+        };
+        const ownProps = {
+          question_id: 42
+        };
+        const componentState = mapStateToProps(appState, ownProps);
+        console.log(componentState);
+        expect(componentState).toEqual(sampleQuestion);
+      });
+    });
+  });
+
+  describe("The display emlement", () => {
+    it(`Should not regress`, () => {
+      const tree = renderer.create(
+        <QuestionDetailDisplay
+          title="The meaning of Life"
+          body="42"
+          answer_count={0}
+          tags={[`philosophy`]}
+        />
+      );
+
+      expect(tree.toJSON()).toMatchSnapshot();
+    });
+  });
 });
